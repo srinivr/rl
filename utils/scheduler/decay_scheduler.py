dc1e54ecb280bd6f75afe18d255b8da9530fd802
srini <srinivas.vr.edu@gmail.com>
2018-05-07 15:16:26 -0400

    refactor

--- utils/scheduler/decay_scheduler.py
+++ utils/scheduler/decay_scheduler.py
@@ -1,11 +1,23 @@
 class StepDecayScheduler:
-    def __init__(self, initial_epsilon=1., lower_bound=0., decay=0.99988):
+    def __init__(self, initial_epsilon=1., lower_bound=0., decay=0.99988, do_decay=True):
         self.epsilon = initial_epsilon
         self.lower_bound = lower_bound
         self.decay = decay
+        self.do_decay = do_decay
+        # TODO log values
 
     def get_epsilon(self, n=None):
         return self.epsilon
 
     def step(self):
-        self.epsilon = max(self.lower_bound, self.decay * self.epsilon)  # decay epsilon every step
+        if self.do_decay:
+            # TODO log values
+            self.epsilon = max(self.lower_bound, self.decay * self.epsilon)
+
+    def set_no_decay(self):
+        # TODO log values
+        self.do_decay = False
+
+    def set_do_decay(self):
+        # TODO log values
+        self.do_decay = True
